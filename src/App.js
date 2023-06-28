95866af61adc2da707843a5507c1c8adda7018d7
fiveseven <44082628+fiveseven-lambda@users.noreply.github.com>
2023-06-28 10:57:58 +0900

    簡単な穴埋めの実現

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let counter = 0;
+  const IFNgamma = ['IFN-γ', ++counter, ...useState(false)];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <Blank>CD4</Blank>陽性のナイーブT細胞は，
+        <Blank>IL-12</Blank>や<Mul props={IFNgamma}/>の存在下でTh1に，
+        <Blank>IL-4</Blank>の存在下でTh2に分化する．
+        Th1は<Mul props={IFNgamma}/>を産生し，
+        Th2は<Blank>IL-4</Blank>，<Blank>IL-5</Blank>，<Blank>IL-9</Blank>，<Blank>IL-13</Blank>を産生する．
+      </p>
     </div>
   );
+}
+function Blank({children}) {
+  const [visible, show] = useState(false);
+  return visible ? <span>{children}</span> : <span onClick={show}> (　) </span>;
+}
+function Mul({props: [str, counter, visible, show]}) {
+  return visible ? <span>{str}</span> : <span onClick={show}> ( {counter} ) </span>;
 }
 
 export default App;
